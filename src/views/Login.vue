4015a82d3867888a3b317422b36080dcc49b4791
lumengxin <2735146963@qq.com>
2020-03-03 22:02:34 +0800

    使用邮箱用户名登录

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,45 +1,62 @@
 <template>
-	<div class="login">
-		<h3>Please login with your google account to continue!</h3>
-		<button @click="login">Login with google</button>
-	</div>
+  <div class="container">
+    <h3>用户登录</h3>
+    <form action="javascript:;">
+      <div class="form-group">
+        <label for="exampleInputEmail1">邮箱：</label>
+        <input
+          v-model="userForm.email"
+          type="email"
+          class="form-control"
+          id="exampleInputEmail1"
+          aria-describedby="emailHelp"
+        />
+        <small
+          id="emailHelp"
+          class="form-text text-muted"
+        >We'll never share your email with anyone else.</small>
+      </div>
+      <div class="form-group">
+        <label for="exampleInputPassword1">密码：</label>
+        <input
+          v-model="userForm.password"
+          type="password"
+          class="form-control"
+          id="exampleInputPassword1"
+        />
+      </div>
+
+      <button type="submit" class="btn btn-primary" @click="login">登录</button>
+    </form>
+  </div>
 </template>
 
 <script>
 import firebase from 'firebase'
 
 export default {
-	name: 'Login',
-	data() {
-		return {}
-	},
-	methods: {
-		login() {
-			var provider = new firebase.auth.GoogleAuthProvider()
-			provider.addScope('https://www.googleapis.com/auth/contacts.readonly')
-
-			firebase
-				.auth()
-				.signInWithPopup(provider)
-				.then(result => {
-					// This gives you a Google Access Token. You can use it to access the Google API.
-					var token = result.credential.accessToken
-					// The signed-in user info.
-					var user = result.user
-          
-          this.$router.push('/')
-				})
-				.catch(function(error) {
-					// Handle Errors here.
-					var errorCode = error.code
-					var errorMessage = error.message
-					// The email of the user's account used.
-					var email = error.email
-					// The firebase.auth.AuthCredential type that was used.
-					var credential = error.credential
-					// ...
-				})
-		}
-	}
+  name: 'Login',
+  data() {
+    return {
+      userForm: {
+        email: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    login() {
+      firebase
+        .auth()
+        .signInWithEmailAndPassword(this.userForm.email, this.userForm.password)
+        .catch(function(error) {
+          // Handle Errors here.
+          var errorCode = error.code
+          var errorMessage = error.message
+          // ...
+		})
+		this.$router.push('/')
+    }
+  }
 }
 </script>
